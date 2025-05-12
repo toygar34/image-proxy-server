22022fdcdb9753830be2798a4cfa6d3c48300bc1
Toygar Ates <toygar34@gmail.com>
2025-05-12 12:32:43 +0300

    Switch to env-based service account

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "roddle-c68bb.firebasestorage.app" // ← kendi Firebase bucket adın olmalı
+  storageBucket: "roddle-c68bb.firebasestorage.app"
 });
 
 const bucket = admin.storage().bucket();
-
 module.exports = bucket;
